0a68a31a99de49f32039f68bfe6fb543bd10c0c0
peer <masterfruitieapple@hotmail.com>
2021-12-01 18:19:42 +0100

    added a bunch of data

---
+++ Assets/Scripts/DisplayManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate(1920,1080,60);
+        }
+    }
+}
