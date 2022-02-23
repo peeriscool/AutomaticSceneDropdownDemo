a7c7f4d7f33e7b2d8fe8607a7463e579df3e9334
peeriscool <masterfruitieapple@hotmail.com>
2022-02-23 11:32:13 +0100

    replaced obselete code & build try
    ad to remove the fabrics script as it was giving a compile error and script was not in use anyway

--- Assets/Scripts/Peer scripts/ParticleOwner.cs
+++ Assets/Scripts/Peer scripts/ParticleOwner.cs
@@ -13,9 +13,9 @@ public class ParticleOwner : MonoBehaviour
     {
         controlledSystem = this.GetComponent<ParticleSystem>();
 
-        if (_particles == null || _particles.Length < controlledSystem.maxParticles)
+        if (_particles == null || _particles.Length < controlledSystem.main.maxParticles)
         {
-            _particles = new ParticleSystem.Particle[controlledSystem.maxParticles];
+              _particles = new ParticleSystem.Particle[controlledSystem.main.maxParticles];
         }
 
     }
@@ -26,7 +26,8 @@ public class ParticleOwner : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.X))
         {
             //stop particle system
-            controlledSystem.enableEmission =! controlledSystem.enableEmission;
+            var emmision = controlledSystem.emission;
+            emmision.enabled =! controlledSystem.emission.enabled;
         }
         if (Input.GetKeyDown(KeyCode.Z))
         {
@@ -38,11 +39,13 @@ public class ParticleOwner : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Minus))
         {
-            controlledSystem.playbackSpeed = controlledSystem.playbackSpeed / 2;
+            var main = controlledSystem.main;
+            main.simulationSpeed = controlledSystem.main.simulationSpeed / 2; 
         }
         if (Input.GetKeyDown(KeyCode.Equals))
         {
-            controlledSystem.playbackSpeed = controlledSystem.playbackSpeed * 2;
+            var main = controlledSystem.main;
+            main.simulationSpeed = controlledSystem.main.simulationSpeed * 2;
         }
 
         if (Input.GetKeyDown(KeyCode.V))
@@ -51,9 +54,11 @@ public class ParticleOwner : MonoBehaviour
             int numparticlesAlive = controlledSystem.GetParticles(_particles);
             for (int i = 0; i < numparticlesAlive; i++)
             {
-               
-                _particles[i].size = Mathf.InverseLerp(_particles[i].size * _particles[i].lifetime, 0, _particles[i].size/2);
-                
+
+#pragma warning disable CS0618 // Type or member is obsolete
+                _particles[i].size = Mathf.InverseLerp(_particles[i].size * _particles[i].remainingLifetime, 0, _particles[i].size/2);
+#pragma warning restore CS0618 // Type or member is obsolete
+
             }
             controlledSystem.SetParticles(_particles);
         }
