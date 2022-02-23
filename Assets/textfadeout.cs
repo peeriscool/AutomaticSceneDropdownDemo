5ae2caffb06d3b6fa1d2e0b2f32fcf19edd003cb
peeriscool <masterfruitieapple@hotmail.com>
2022-02-23 15:55:34 +0100

    Fixed Pauze tekst fade in and out

--- Assets/textfadeout.cs
+++ Assets/textfadeout.cs
@@ -6,13 +6,17 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Text))]
 public class textfadeout : MonoBehaviour
 {
-    public bool Invert;
+    public bool hide_at_start;
     Text manip;
     float time;
     // Start is called before the first frame update
     void Start()
     {
         manip = this.gameObject.GetComponent<Text>();
+        if(hide_at_start == true)
+        {
+            manip.enabled = false;
+        }
     }
 
     // Update is called once per frame
@@ -20,15 +24,22 @@ public class textfadeout : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-         if (Invert)
-          {
-                StartCoroutine(FadeAlphaToFullText(3f, manip));
-            }
-            else
-            {
-                StartCoroutine(FadeTextToFullAlpha(3f, manip));        
-            }
-     }
+                manip.enabled = true;
+                Debug.Log("Text Should appear");
+                StartCoroutine(FadeTextToFullAlpha(3f, manip));
+        }
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+            manip.enabled = false;
+        }
+        if(Input.GetKeyDown(KeyCode.U))
+        {
+            Debug.Log("Text Should disappear");
+             StartCoroutine(FadeAlphaToFullText(3f, manip));
+        }
+
+
+
         //  float alpha = Mathf.Lerp(0,10,Time.deltaTime);
         //   Material alphafade = manip.material;
         // alphafade.color = new Color(alphafade.color.r, alphafade.color.g, alphafade.color.b, Mathf.Lerp(0, 10, Time.deltaTime));
@@ -37,7 +48,7 @@ public class textfadeout : MonoBehaviour
     public IEnumerator FadeTextToFullAlpha(float t, Text i)
     {
         i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
-        while (i.color.a < 0.0f)
+        while (i.color.a < 1.0f)
         {
             i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
             yield return null;
@@ -47,9 +58,9 @@ public class textfadeout : MonoBehaviour
     public IEnumerator FadeAlphaToFullText(float t, Text i)
     {
         i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
-        while (i.color.a < 1.0f)
+        while (i.color.a > 0.0f)
         {
-            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime * t));
             yield return null;
         }
     }
